3d862afcc800cd6651120d305e518752a3dd2b78
蚊子 <634439817@qq.com>
2023-04-15 13:16:03 +0800

    typing

--- Assets/Scripts/Typing.cs
+++ Assets/Scripts/Typing.cs
@@ -8,17 +8,16 @@ public class Typing : MonoBehaviour
 {
     public TextMeshProUGUI originalText;
     public TextMeshProUGUI typedText;
-    public TextMeshProUGUI cursor;
-    public RectTransform cursorRectTransform;
-
+   
 
     public string targetText;  //玩家需要输入的目标文本
     private int currentCharIndex;  //当前需要输入的字符索引
 
-    //光标闪烁
-    private float cursorBlinkInterval = 0.5f;
-    private float cursorBlinkTimer;
-    private bool cursorVisible;
+    //待输入文字变色，文字闪烁
+    public Color newColor;
+
+    public float blinkDuration = 0.75f;
+    private bool isBlinking;
 
 
     // Start is called before the first frame update
@@ -27,68 +26,69 @@ public class Typing : MonoBehaviour
         targetText = "AABBFFCCCCCCCCCCCCCCCC";
         currentCharIndex = 0;
         originalText.text = targetText;
-        typedText.text = "";
-        cursor.text = "|";
-        cursorBlinkTimer = 0;
-        cursorVisible = true;
+        typedText.text = "";       
+
+        //ChangeColorAt(currentCharIndex, newColor);
+        StartCoroutine(BlinkChar());
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-        if (currentCharIndex < targetText.Length)
+       
+
+        targetText = targetText.Replace("<color=#FFFFFF>", "");
+        targetText = targetText.Replace("</color>", "");
+        char currentChar = targetText [currentCharIndex];
+        if (Input.GetKeyDown((KeyCode)((int)currentChar + 32)))
         {
-            char currentChar = targetText[currentCharIndex];
-            if (Input.GetKeyDown((KeyCode)((int)currentChar + 32)))
+            if (currentCharIndex < targetText.Length)
             {
+                Debug.Log("按下按键");
                 typedText.text += currentChar;
-                currentCharIndex++;
+                currentCharIndex++; 
+                StartCoroutine(BlinkChar());
+                
+                //ChangeColorAt(currentCharIndex, newColor);
 
-                // 更新光标位置
-                //typedText.ForceMeshUpdate();
-                TMP_TextInfo textInfo = originalText.textInfo;
-                Debug.Log(textInfo.characterCount);
-                Debug.Log(currentCharIndex);
-                if (currentCharIndex < textInfo.characterCount)
-                {
-                    Debug.Log("更新位置");
+            }
+            else
+            {
+                Debug.Log("输入已满");
+            }
+            
+        }
 
-                    TMP_CharacterInfo charInfo = textInfo.characterInfo[currentCharIndex - 1];
-                    //float cursorXPos = charInfo .topRight.x-367;
-                    //float cursorYPos = charInfo.bottomLeft.y+40;
+    }
 
-                    float charWidth = charInfo.xAdvance; // 获取字符的宽度
-                    float cursorXPos = charInfo.bottomLeft.x + charWidth; // 将字符宽度加到光标的X坐标上
-                    float cursorYPos = charInfo.bottomLeft.y; // 保持光标的Y坐标不变
+    public void ChangeColorAt(int index, Color color)
+    {
+        string originStr = targetText;
+        string colorHexCode = ColorUtility.ToHtmlStringRGBA(color);
+        string coloredChar = $"<color=#{colorHexCode}>{originStr[index]}</color>";
+        originalText.text = originStr.Substring(0, index) + coloredChar + originStr.Substring(index + 1);
+    }
 
+    private IEnumerator BlinkChar()
+    {
+        isBlinking = true;
+        
+        while (isBlinking)
+        {
+            if (currentCharIndex >= 0 && currentCharIndex < targetText.Length)
+            {
+                originalText.text = targetText.Substring(0, currentCharIndex) + "<color=#0000>" + targetText[currentCharIndex] + "</color>" + targetText.Substring(currentCharIndex + 1);
+                yield return new WaitForSeconds(blinkDuration);
 
-                    int lineIndex = charInfo.lineNumber;
+                originalText.text = targetText;
+                yield return new WaitForSeconds(blinkDuration);
 
-                    if (lineIndex > 0)
-                    {
-                        cursorYPos = textInfo.lineInfo[lineIndex].ascender;
-                    }
-                    Debug.Log(cursorXPos);
-                    Debug.Log(cursorYPos);
-                    cursorRectTransform.anchoredPosition = new Vector2(cursorXPos, cursorYPos);
-                   
-
-                }
+            }
+            else
+            {
+                Debug.LogError("Character index is out of range.");
+                isBlinking = false;
             }
         }
-
-        // 光标闪烁逻辑
-        cursorBlinkTimer += Time.deltaTime;
-        if (cursorBlinkTimer >= cursorBlinkInterval)
-        {
-            cursorBlinkTimer = 0;
-            cursorVisible = !cursorVisible;
-            cursor.enabled = cursorVisible;
-        }
-
-
-
     }
 }
